20f8f21bc9e72e442eb497d47605e0af98db5a23
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-04-18 19:12:13 -0400

    Fix ffi native
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 moon update && moon install && rm -rf target
+moon add tonyfettes/uv
+moon add peter-jerry-ye/async
+
 moon fmt && moon info --target native
 moon test --target native
 # moon test --target all
