62656ac9bfc73ce52ba889b46688e13f19c6a2e3
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-06-10 08:18:34 -0400

    Update to moonc v0.1.20250606+a3f4966ca
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 moon update && moon install && rm -rf target
-moon add tonyfettes/uv
+# moon add tonyfettes/uv - "error: Multiple conflicting versions were found for module tonyfettes/encoding: [Version { major: 0, minor: 3, patch: 1 }, Version { major: 0, minor: 1, patch: 0 }]"
 moon add peter-jerry-ye/async
 
 moon fmt && moon info --target native
